b605001d0146851e481c33ebdcedd339d5b0af31
moli <2720316926@qq.com>
2026-04-06 16:37:42 +0800

    feat: add batch sending to avoid Feishu card 100KB size limit

--- lark_post.py
+++ lark_post.py
@@ -12,61 +12,67 @@ def post_to_lark_webhook(tag: str, papers: list, config: dict):
         'Content-Type': 'application/json'
     }
 
-    # Card Template Data
-    today_date = datetime.date.today().strftime('%Y-%m-%d')
-    table_rows = [
-        {
-            "index": i + 1,
-            "title": paper['title'],
-            "id": paper['id'],
-            "published": paper['published'],
-            "url": f"[{paper['url']}]({paper['url']})"
-        }
-        for i, paper in enumerate(papers)
-    ]
-    paper_list = [
-        {
-            "counter": i + 1,
-            "title": paper['title'],
-            "id": paper['id'],
-            "abstract": paper['abstract'],
-            "zh_abstract": paper.get('zh_abstract', None),
-            "url": paper['url'],
-            "published": paper['published']
-        }
-        for i, paper in enumerate(papers)
-    ]
+    # 因为飞书卡片有 100KB 容量限制，如果论文太多（比如超过10篇连带长摘要），很容易超限
+    # 将长列表进行分批发送，每批最多 10 篇
+    batch_size = 10
+    total_batches = (len(papers) + batch_size - 1) // batch_size
 
-    card_data = {
-        "type": "template",
-        "data": {
-            "template_id": config['template_id'],
-            "template_version_name": config['template_version_name'],
-            "template_variable": {
-                "today_date": today_date,
-                "tag": tag,
-                "total_paper": len(papers),
-                "table_rows": table_rows,
-                "paper_list": paper_list
+    for batch_idx in range(total_batches):
+        batch_papers = papers[batch_idx * batch_size : (batch_idx + 1) * batch_size]
+        
+        # 组装这批卡片数据
+        today_date = datetime.date.today().strftime('%Y-%m-%d')
+        table_rows = [
+            {
+                "index": i + 1 + batch_idx * batch_size,
+                "title": paper['title'],
+                "id": paper['id'],
+                "published": paper['published'],
+                "url": f"[{paper['url']}]({paper['url']})"
+            }
+            for i, paper in enumerate(batch_papers)
+        ]
+        paper_list = [
+            {
+                "counter": i + 1 + batch_idx * batch_size,
+                "title": paper['title'],
+                "id": paper['id'],
+                "abstract": paper['abstract'],
+                "zh_abstract": paper.get('zh_abstract', None),
+                "url": paper['url'],
+                "published": paper['published']
+            }
+            for i, paper in enumerate(batch_papers)
+        ]
+
+        card_data = {
+            "type": "template",
+            "data": {
+                "template_id": config['template_id'],
+                "template_version_name": config['template_version_name'],
+                "template_variable": {
+                    "today_date": f"{today_date} (Part {batch_idx + 1}/{total_batches})" if total_batches > 1 else today_date,
+                    "tag": tag,
+                    "total_paper": len(batch_papers),
+                    "table_rows": table_rows,
+                    "paper_list": paper_list
+                }
             }
         }
-    }
 
-    data = {
-        "msg_type": "interactive",
-        "card": card_data
-    }
+        data = {
+            "msg_type": "interactive",
+            "card": card_data
+        }
 
-    # Send HTTP POST request
-    response = requests.post(config['webhook_url'], headers=headers, data=json.dumps(data))
+        # Send HTTP POST request
+        response = requests.post(config['webhook_url'], headers=headers, data=json.dumps(data))
 
-    if response.status_code == 200:
-        print("Request successful")
-        print("Response:\n{}".format(response.json()))
-    else:
-        print("Request failed, status code: {}".format(response.status_code))
-        print("Response:\n{}".format(response.text))
-
+        if response.status_code == 200:
+            print(f"Batch {batch_idx + 1}/{total_batches} - Request successful")
+        else:
+            print(f"Batch {batch_idx + 1}/{total_batches} - Request failed, status code: {response.status_code}")   
+            print("Response:\n{}".format(response.text))
 
 if __name__ == '__main__':
     papers = [
